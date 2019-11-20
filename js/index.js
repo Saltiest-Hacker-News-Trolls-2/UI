aa964211e1a296c6c4f1b0c1665b81f4cc7c6dcc
agohorel <gohorelalex@gmail.com>
2019-11-20 18:12:08 -0500

    fix window resizing mobile nav jank

--- js/index.js
+++ js/index.js
@@ -3,6 +3,18 @@ const hamburger = document.querySelector(".hamburger");
 const hamburgerIcon = document.querySelector(".hamburger-icon");
 
 hamburger.addEventListener("click", function() {
-  mobileNav.classList.toggle("mobile-nav--slide");
   hamburger.classList.toggle("hamburger-pressed");
+
+  if (
+    !mobileNav.classList.contains("mobile-nav--slide-out") &&
+    !mobileNav.classList.contains("mobile-nav-slide")
+  ) {
+    mobileNav.classList.add("mobile-nav--slide-out");
+  } else if (mobileNav.classList.contains("mobile-nav--slide-out")) {
+    mobileNav.classList.remove("mobile-nav--slide-out");
+    mobileNav.classList.add("mobile-nav--slide");
+  } else if (mobileNav.classList.contains("mobile-nav--slide")) {
+    mobileNav.classList.remove("mobile-nav--slide");
+    mobileNav.classList.add("mobile-nav--slide-out");
+  }
 });
